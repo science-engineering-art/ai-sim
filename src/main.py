8c2847561b17567e011f9b9e2e6b503976a410ed
Marcos <marcosmath44@gmail.com>
2022-12-23 13:28:52 -0500

    Added curve Roads

--- src/main.py
+++ src/main.py
@@ -13,18 +13,40 @@ pygame.init()
 screen = pygame.display.set_mode((1400,800))
 pygame.display.update()
 
-roads = [
-    Road((0,400),(700,400)),
-    Road((700,400),(1400,400))
+def draw_road(screen, road : Road, color):
+    x, y = road.start
+    l = road.length
+    h = 10
+    vertices = [(x + (v1*l*road.angle_cos + v2*h*road.angle_sin)/2, y + (v1*l*road.angle_sin - v2*h*road.angle_cos)/2) for v1,v2 in [(0,-1), (0,1), (2,1), (2,-1)]]
+
+    gfxdraw.filled_polygon(screen, vertices, color)
+    
+
+def createRoads(pair_point_list):
+    
+    roads = []
+    for x, y in pair_point_list:
+        roads.append(Road(x,y))
+        
+    return roads
+    
+
+road_locations = [
+    ((0,400),(700,400)),
+    ((710,410),(710,900)),
+    # Road((700,400),(1400,400))
+    *Road.get_curve_road((700,400), (710,410), (710, 400))
 ]
 
-car = Vehicle(650, 401, 14,7)
-car.stopped = True
-vehicles = [
-    car,
-    Vehicle(60,401,14,7),
-    Vehicle(40,401,14,7)
-]
+roads = [Road(x,y) for x,y in road_locations]
+
+# car = Vehicle(650, 401, 14,7)
+# car.stopped = True
+# vehicles = [
+#     car,
+#     Vehicle(60,401,14,7),
+#     Vehicle(40,401,14,7)
+# ]
 
 running = True
 count = 0
@@ -32,20 +54,21 @@ while running:
 
     for event in pygame.event.get():
         if event.type == QUIT:
+            pygame.quit()
             # running = False
-            car.stopped = False
+            # car.stopped = False
         print(event)
     
     screen.fill((220,220,220))
     
     for road in roads:
-        gfxdraw.box(screen, road.get_rect, GRAY)
+        draw_road(screen,road, GRAY)
     
-    vehicles[0].update()
-    gfxdraw.box(screen, vehicles[0].get_rect, BLUE)
-    for i in range(1, len(vehicles)):
-        vehicles[i].update(1/60, vehicles[i-1])
-        gfxdraw.box(screen, vehicles[i].get_rect, BLUE)
+    # vehicles[0].update()
+    # gfxdraw.box(screen, vehicles[0].get_rect, BLUE)
+    # for i in range(1, len(vehicles)):
+    #     vehicles[i].update(1/60, vehicles[i-1])
+    #     gfxdraw.box(screen, vehicles[i].get_rect, BLUE)
     
     pygame.display.update()
 
--- src/models/road.py
+++ src/models/road.py
@@ -1,13 +1,42 @@
+from ctypes import create_unicode_buffer
 from typing import List, Tuple
+from venv import create
+from scipy.spatial import distance
 
 
 class Road:
     def __init__(self, start:Tuple[int,int], end: Tuple[int,int]):
         self.start = start
         self.end = end
+        self.length = distance.euclidean(self.start, self.end)
+        self.angle_sin = (self.end[1]-self.start[1]) / self.length
+        self.angle_cos = (self.end[0]-self.start[0]) / self.length
 
     @property
     def get_rect(self):
         x0,y0 = self.start
         x1,_ = self.end
         return (x0, y0, x1-x0, 10)
+    
+    def get_curve_road(init_point, end_point, inclination_point, steps = 15):
+        points = Road.create_curve_road_points(init_point, end_point, inclination_point, steps)
+        
+        roads = []
+        for i in range(len(points) - 1):
+            roads.append((points[i], points[i+1]))
+            
+        return roads
+    
+    def create_curve_road_points(init_point, end_point, inclination_point, steps = 40):
+        points = []
+        
+        for i in range(steps + 1):
+            t = i/steps
+            x = init_point[0] * (1-t)**2 + 2 * (1-t) * t * inclination_point[0] + t**2 * end_point[0]
+            y = init_point[1] * (1-t)**2 + 2 * (1-t) * t * inclination_point[1] + t**2 * end_point[1]
+            points.append((x,y))   
+        
+        return points
+            
+            
+
