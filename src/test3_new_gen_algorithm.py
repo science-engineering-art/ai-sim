33bcea1d9888c0ff8840434d053fdd5197b90b96
Marcos <marcosmath44@gmail.com>
2023-01-02 06:57:47 -0500

    Update test3_new_gen_algorithm.py

--- src/test3_new_gen_algorithm.py
+++ src/test3_new_gen_algorithm.py
@@ -8,6 +8,7 @@ from models.simulation import Simulation, Simulation_test_3
 
 simulation = Simulation_test_3()
 MAX_ITERATIONS = 10
-genetic_algorithm(simulation, 10, 15, 100, 5,speed = 20, obs_time=1, max_iterations= 5)
+genetic_algorithm(simulation, pop_size=10, number_of_turns=15, maximum_waiting_time=100, 
+                  average_passing_time=5,speed = 20, obs_time=1, max_iterations= 5)
 
 
