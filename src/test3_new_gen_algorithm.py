b3cf0e961cc884aa779424a61e3b185ec5d2b101
LRiera24 <laura.riera2412@gmail.com>
2023-01-02 13:40:41 -0500

    test 100 iter w 30 pop size and 1 obs time
    fitness = 0 found

--- src/test3_new_gen_algorithm.py
+++ src/test3_new_gen_algorithm.py
@@ -8,13 +8,13 @@ from models.simulation import Simulation, Simulation_test_3
 
 simulation = Simulation_test_3()
 
-pop_size = 5
+pop_size = 30
 number_of_turns = 15
 maximum_waiting_time = 90
 average_passing_time = 3
 speed = 20
 obs_time = 1
-max_iterations = 10
+max_iterations = 100
 gam.genetic_algorithm(simulation, pop_size, number_of_turns,
                       maximum_waiting_time, average_passing_time, speed, obs_time, max_iterations)
 # ga.genetic_algorithm(simulation, pop_size, number_of_turns, maximum_waiting_time, average_passing_time)
